683551f43c5aa396a1615154f9a2978f80e27828
emontesinos812 <emontesinos812@gmail.com>
2018-07-10 12:06:25 -0300

    personalizando marcador de mapa

--- app/src/main/java/com/example/eddymontesinos/demogoglemaps/MapsFragment.kt
+++ app/src/main/java/com/example/eddymontesinos/demogoglemaps/MapsFragment.kt
@@ -9,6 +9,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -26,9 +28,26 @@ class MapsFragment: SupportMapFragment() ,OnMapReadyCallback {
 
     override fun onMapReady( map: GoogleMap?) {
         this.mapa = map
+        val marcador : MarkerOptions?
         val peru = LatLng(-11.927467033443245,-76.99462369988953)
-         mapa?.addMarker(MarkerOptions().position(peru).title("PERU.Lima"))
-        mapa?.moveCamera(CameraUpdateFactory.newLatLng(peru))
+        //añadimos la posicion del marcador y le pasamos el titulo
+         //mapa?.addMarker(MarkerOptions().position(peru).title("PERU.Lima"))
+        marcador = MarkerOptions()
+        marcador.position(peru)
+        marcador.title("PERU.Lima")
+        marcador.snippet("Super Mercado")
+        marcador.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_edit_location_black_24px))
+        mapa?.addMarker(marcador)
+
+        val camara = CameraPosition.Builder()
+                .target(peru)
+                .zoom(5f)//
+                .bearing(0f)//rotacion que se puede dar ala camara 0 a maximo de 365grados
+                .tilt(0f)//angulo de nuestro marcador 0 a 90 grados
+                .build()
+        mapa?.animateCamera(CameraUpdateFactory.newCameraPosition(camara))
+        //movemos la camara al ñpunto del marcador
+         //mapa?.moveCamera(CameraUpdateFactory.newLatLng(peru))
 
     }
 
