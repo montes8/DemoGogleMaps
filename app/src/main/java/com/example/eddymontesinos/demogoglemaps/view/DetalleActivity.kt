b2b2ab04ff48a7810e95ba3ea6f7e2f5b64dd893
emontesinos812 <emontesinos812@gmail.com>
2018-07-10 08:14:34 -0300

    Implementado toolbar al detalle

--- app/src/main/java/com/example/eddymontesinos/demogoglemaps/view/DetalleActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demogoglemaps/view/DetalleActivity.kt
@@ -3,6 +3,7 @@ package com.example.eddymontesinos.demogoglemaps.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.eddymontesinos.demogoglemaps.R
+import kotlinx.android.synthetic.main.activity_detalle.*
 
 class DetalleActivity : AppCompatActivity() {
 
@@ -14,5 +15,15 @@ class DetalleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
+    }
+
+
+    fun ajustarTollbarDetalle(){
+        setSupportActionBar(detalleToolbar)
+        title = "Detalles"
+        detalleToolbar.navigationIcon = getDrawable(R.drawable.ic_atras)
+        detalleToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
