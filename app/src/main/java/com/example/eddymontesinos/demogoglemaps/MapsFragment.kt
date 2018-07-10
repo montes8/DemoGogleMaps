26fbed4c3474a5e8f8a87c28d9e629a1b0e5e73a
emontesinos812 <emontesinos812@gmail.com>
2018-07-10 16:48:51 -0300

    enfocando vista mapa a los marker

--- app/src/main/java/com/example/eddymontesinos/demogoglemaps/MapsFragment.kt
+++ app/src/main/java/com/example/eddymontesinos/demogoglemaps/MapsFragment.kt
@@ -6,34 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eddymontesinos.demogoglemaps.model.Mercado
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.VectorDrawable
 import android.os.Build
+import android.os.Handler
 import android.support.v7.content.res.AppCompatResources
+import com.example.eddymontesinos.demogoglemaps.view.DetalleActivity
 import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.BitmapDescriptor
-
+import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.CameraUpdate
+import org.jetbrains.anko.support.v4.startActivity
 
 
 class MapsFragment: SupportMapFragment() ,OnMapReadyCallback {
 
     var mapa: GoogleMap? = null
-    var listasupermercados : ArrayList<Mercado>? = null
+    var handler : Handler = Handler()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = super.onCreateView(inflater, container, savedInstanceState)
         getMapAsync(this)
-
-         listasupermercados = ArrayList<Mercado>()
-        listasupermercados?.add(Mercado("Royal Plaza",-11.99041933,-77.06290144))
-        listasupermercados?.add(Mercado("Mega Plaza",-11.99405732,-77.06241231))
-        listasupermercados?.add(Mercado("Plaza Norte",-12.00681565,-77.05884285))
-        listasupermercados?.add(Mercado("Real Plaza",-12.05688609,-77.03773475))
-        listasupermercados?.add(Mercado("Ramba Brasil",-12.06631014,-77.04746379))
 
 
         return rootView
@@ -41,37 +35,53 @@ class MapsFragment: SupportMapFragment() ,OnMapReadyCallback {
 
     override fun onMapReady( map: GoogleMap?) {
         this.mapa = map
-        val marcador : MarkerOptions?
-        val superIcono = getBitmapDescriptor(R.drawable.ic_shopping)
-        val peru = LatLng(-11.927467033443245,-76.99462369988953)
+
+
+        val lima = LatLng(-11.97650551576034,-77.05710844110047)
         mapa!!.uiSettings.isZoomControlsEnabled = true
+        mapa!!.uiSettings.isCompassEnabled= true
         //añadimos la posicion del marcador y le pasamos el titulo
-         mapa?.addMarker(MarkerOptions().position(peru).title("PERU.Lima"))// se gregaga para k sea arrastrable isDraggable(true)
-        //personalñizandomarcador customisando
-    /*    marcador = MarkerOptions()
-        marcador.position(peru)
-        marcador.title("PERU.Lima")
-        marcador.snippet("Super Mercado")/cuadro de subtitulo o descripcion
-        marcador.icon(BitmapDescriptorFactory.fromResource(android.R.drawable.arrow_down_float))
-        mapa?.addMarker(marcador)*/
+         //mapa?.addMarker(MarkerOptions().position(lima).title("PERU.Lima"))// se gregaga para k sea arrastrable isDraggable(true)
 
-      listasupermercados?.forEach {
-          mapa?.addMarker(MarkerOptions().position(LatLng(it.latitud,it.longitud))
-                  .title(it.nombre)
-                  .icon(superIcono))
-      }
+        val builder = LatLngBounds.Builder()
 
-        val camara = CameraPosition.Builder()
-                .target(peru)
-                .zoom(5f)//vista deacuerdo a pais
+        Thread{
+            val lista = DemoApplication.database!!.superMercadoDao().litarSuperMercados()
+
+            handler.post {
+
+                lista.forEach {
+
+                    val marker = mapa?.addMarker(MarkerOptions().position(LatLng(it.latitud,it.longitud)).title(it.nombre))
+                    //marker?.tag = it
+                    builder.include(marker?.position)
+
+                    val bounds : LatLngBounds= builder.build()
+                    val padding = 200 // offset from edges of the map in pixels
+                    val cu :CameraUpdate= CameraUpdateFactory.newLatLngBounds(bounds,padding)
+                    mapa?.moveCamera(cu)
+                }
+            }
+        }.start()
+
+          mapa?.setOnMarkerClickListener(object :GoogleMap.OnMarkerClickListener{
+              override fun onMarkerClick(p0: Marker?): Boolean {
+                  startActivity<DetalleActivity>()
+                  return true
+              }
+
+          })
+      /* val camara = CameraPosition.Builder()
+                .target(lima)
+                .zoom(10f)//vista deacuerdo a pais
                 .bearing(0f)//rotacion que se puede dar ala camara 0 a maximo de 365grados
-                .tilt(0f)
-                //angulo de nuestro marcador 0 a 90 grados
-                .build()
-        mapa?.animateCamera(CameraUpdateFactory.newCameraPosition(camara))
+                .tilt(0f)//angulo de nuestro marcador 0 a 90 grados
+                .build()*/
+        //mapa?.animateCamera(CameraUpdateFactory.newCameraPosition(camara))
 
         //movemos la camara al ñpunto del marcador
-         //mapa?.moveCamera(CameraUpdateFactory.newLatLng(peru))
+       // mapa?.moveCamera(CameraUpdateFactory.newLatLng(lima))
+
 
     }
 
