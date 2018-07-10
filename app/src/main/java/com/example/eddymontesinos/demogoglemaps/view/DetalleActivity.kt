4b7637cc86fec10ce8fcf8366f112b28fcf03f57
emontesinos812 <emontesinos812@gmail.com>
2018-07-10 08:24:33 -0300

    pasando datos al activity detalle

--- app/src/main/java/com/example/eddymontesinos/demogoglemaps/view/DetalleActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demogoglemaps/view/DetalleActivity.kt
@@ -2,7 +2,9 @@ package com.example.eddymontesinos.demogoglemaps.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.eddymontesinos.demogoglemaps.R
+import com.example.eddymontesinos.demogoglemaps.model.SuperMercado
 import kotlinx.android.synthetic.main.activity_detalle.*
 
 class DetalleActivity : AppCompatActivity() {
@@ -15,6 +17,20 @@ class DetalleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
+        ajustarTollbarDetalle()
+        val detallesSuperMercado = intent.getParcelableExtra<SuperMercado>(SUPERMERCADO_PARAM)
+
+        val nombre = findViewById<TextView>(R.id.text_detail_nombre)
+        val latitud = findViewById<TextView>(R.id.text_detail_latitude)
+        val longitud = findViewById<TextView>(R.id.text_detail_longitude)
+        val direccion = findViewById<TextView>(R.id.text_detail_direccion)
+
+        text_detail_nombre.text = detallesSuperMercado.nombre
+        text_detail_latitude.text = detallesSuperMercado.latitud.toString()
+        text_detail_longitude.text = detallesSuperMercado.longitud.toString()
+        text_detail_direccion.text = detallesSuperMercado.direccion
+
+
     }
 
 
