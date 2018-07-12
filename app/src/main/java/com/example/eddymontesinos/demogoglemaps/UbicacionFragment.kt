6911faf3f488bc46f314bae421bad7e466d8385f
emontesinos812 <emontesinos812@gmail.com>
2018-07-12 14:55:39 -0300

    utilizando localizacion de gogle

--- app/src/main/java/com/example/eddymontesinos/demogoglemaps/UbicacionFragment.kt
+++ app/src/main/java/com/example/eddymontesinos/demogoglemaps/UbicacionFragment.kt
@@ -10,32 +10,49 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.os.ProxyFileDescriptorCallback
 import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_ubicacion.*
 
 
-class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback , LocationListener{
+class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback {
+
 
     companion object {
         private const val PERMISO_LOCATION = 1
+        private const val UPDATE_INTERVAL: Long = 15000  /* 15 secs */
+        private const val FASTEST_INTERVAL: Long = 5000 /* 5 secs */
     }
     var mapagps: GoogleMap? = null
-    var localizacion : LocationManager? = null
-    var marcador : Marker? = null
+    var marker : Marker? = null
+    var fusedLocationProviderClient : FusedLocationProviderClient? = null
+    var locationrequet : LocationRequest? = null
+    var locationCallback : LocationCallback? = null
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = super.onCreateView(inflater, container, savedInstanceState)
         getMapAsync(this)
+
+
 
         return rootView
     }
@@ -43,43 +60,49 @@ class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback , LocationLi
     @SuppressLint("MissingPermission")
     override fun onMapReady(map: GoogleMap?) {
         this.mapagps = map
-        localizacion = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ActivityCompat.checkSelfPermission(context!!, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                //mapagps!!.isMyLocationEnabled = true
+                mapagps!!.isMyLocationEnabled = true
                 mapagps!!.uiSettings.isZoomControlsEnabled = true
-                localizacion!!.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,0f,this)
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+
+                buildLocationrequest()
+                buidlLocationCallback()
+
+                fusedLocationProviderClient!!.requestLocationUpdates(locationrequet, locationCallback, Looper.myLooper())
+
 
             } else {
                 requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISO_LOCATION)
               }
         }else{
 
-            //mapagps!!.isMyLocationEnabled = true
+            mapagps!!.isMyLocationEnabled = true
             mapagps!!.uiSettings.isZoomControlsEnabled = true
+
         }
     }
 
-    override fun onLocationChanged(location: Location) {
-        Toast.makeText(context,"Actulizado", Toast.LENGTH_LONG).show()
-        if(marcador == null){
-            marcador = mapagps!!.addMarker(MarkerOptions().position(LatLng(location.latitude,location.longitude)).draggable(true))
-        }else{
-            marcador?.position = LatLng(location.latitude,location.longitude)
-        }
+    private fun buidlLocationCallback() {
+       locationCallback = object :LocationCallback(){
+           override fun onLocationResult(po: LocationResult?) {
+               super.onLocationResult(po)
+               val location =po!!.locations.get(po.locations.size-1)
+
+
+               marker = mapagps!!.addMarker(MarkerOptions().position(LatLng(location.latitude,location.longitude)).draggable(true))
+           }
+       }
     }
 
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onProviderEnabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onProviderDisabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun buildLocationrequest() {
+        locationrequet = LocationRequest()
+        locationrequet?.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        locationrequet?.interval = 5000
+        locationrequet?.fastestInterval = 3000
+        locationrequet?.smallestDisplacement = 10f
     }
 
 
@@ -96,7 +119,7 @@ class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback , LocationLi
                     if (resultado == PackageManager.PERMISSION_GRANTED){
                         Log.d("resultado","resultado")
                         Toast.makeText(context,"has dado permiso", Toast.LENGTH_LONG).show()
-                        //mapagps!!.isMyLocationEnabled = true
+                        mapagps!!.isMyLocationEnabled = true
                         mapagps!!.uiSettings.isZoomControlsEnabled = true
                     }else{
                         Toast.makeText(context,"has denegado el permiso", Toast.LENGTH_LONG).show()
