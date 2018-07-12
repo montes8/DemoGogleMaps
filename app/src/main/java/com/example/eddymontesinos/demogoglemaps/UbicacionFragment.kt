25d72a1b7dd6dc830b7a2953da49443b1940fcf2
emontesinos812 <emontesinos812@gmail.com>
2018-07-12 11:15:15 -0300

    implementando LocationListener

--- app/src/main/java/com/example/eddymontesinos/demogoglemaps/UbicacionFragment.kt
+++ app/src/main/java/com/example/eddymontesinos/demogoglemaps/UbicacionFragment.kt
@@ -3,12 +3,11 @@ package com.example.eddymontesinos.demogoglemaps
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.view.LayoutInflater
@@ -18,11 +17,9 @@ import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import org.jetbrains.anko.support.v4.toast
 
 
-class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback{
-
+class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback , LocationListener{
 
     companion object {
         private const val PERMISO_LOCATION = 1
@@ -55,7 +52,21 @@ class UbicacionFragment : SupportMapFragment() , OnMapReadyCallback{
         }
     }
 
+    override fun onLocationChanged(location: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
     @SuppressLint("MissingPermission")
